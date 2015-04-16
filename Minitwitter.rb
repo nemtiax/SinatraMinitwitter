aabe4163ce3c298d318370ce44e8701062f33561
Nick <nemtiax@brandeis.edu>
2015-04-16 15:55:57 -0400

    redis

--- Minitwitter.rb
+++ Minitwitter.rb
@@ -13,7 +13,7 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 before do
 	uri = URI.parse(ENV["REDISTOGO_URL"])
-	REDIS |= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+	REDIS ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
 get '/' do
